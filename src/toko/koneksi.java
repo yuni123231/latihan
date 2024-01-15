5282c09cb7751541238a2663e5fd86d785c1888e
yuni123231 <118106821+yuni123231@users.noreply.github.com>
2024-01-15 21:43:46 +0700

    Add files via upload

---
+++ src/toko/koneksi.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package toko;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static final String URL = "jdbc:mysql://localhost:3306/toko";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+    }
